725f88a5d794f46a32bf5a868ae481e709a23f7c
Martin Delille <martin@phonations.com>
2016-04-14 15:59:20 +0200

    Add BartlebyCLI to Bartleby

---
+++ BartlebyCLI/bartleby/main.swift
@@ -0,0 +1,18 @@
+    //
+//  main.swift
+//  bartleby
+//
+//  Created by Benoit Pereira da silva on 12/01/2016.
+//  Copyright © 2016 Benoit Pereira da silva. All rights reserved.
+//
+
+import Foundation
+
+
+// Instanciate the facade
+let facade=BartlebysCommandFacade()
+facade.actOnArguments()
+
+var holdOn=true
+let runLoop=NSRunLoop.currentRunLoop()
+while (holdOn && runLoop.runMode(NSDefaultRunLoopMode, beforeDate: NSDate.distantFuture()) ){}
