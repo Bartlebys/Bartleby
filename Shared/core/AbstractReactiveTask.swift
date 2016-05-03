62fa6b3fbb539e999344cb224b8a2b1d3ace039a
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2016-05-03 09:33:00 +0200

    Moving promoted classes to core

---
+++ Shared/core/AbstractReactiveTask.swift
@@ -0,0 +1,64 @@
+//
+//  ReactiveAbstractTask.swift
+//  BartlebyKit
+//
+//  Created by Benoit Pereira da silva on 29/04/2016.
+//  Copyright © 2016 Lylo Media Group SA. All rights reserved.
+//
+
+import Foundation
+
+public class  AbstractReactiveTask: Task {
+    
+    /**
+     The convenience intializer that must be overriden.
+     You should always serialize the argument during initialization phasse!
+     
+     - parameter arguments: the arguments to be serialized
+    
+     - returns: the task instance.
+     */
+    convenience required public init (arguments:Serializable) {
+        self.init()
+        self.argumentsData=arguments.serialize()
+    }
+    
+    // MARK: Sequential Tasks
+    
+    internal lazy var lastSequentialTask:Task=self
+    
+    /**
+     Appends a task to the last sequential task
+     
+     - parameter task: the task to be sequentially added
+     */
+    public func appendSequentialTask(task:Task){
+        lastSequentialTask.addChildren(task)
+        lastSequentialTask=task
+    }
+    
+    
+    // MARK: Reactive Handlers
+    
+    private var _reactiveHandlers:ProgressAndCompletionHandler?
+    
+    // The reactive Handlers
+    public var reactiveHandlers:ProgressAndCompletionHandler{
+        get{
+            if let _ = _reactiveHandlers {
+                return self._reactiveHandlers!
+            }
+            let onCompletion:CompletionHandler={ (completionState) in
+                // We forward the completion to the scheduler.
+                self.forward(completionState)
+                NSNotificationCenter.defaultCenter().postNotification(completionState.completionNotification)
+            }
+            self._reactiveHandlers=ProgressAndCompletionHandler(completionHandler: onCompletion)
+            self._reactiveHandlers!.addProgressHandler({ (progressionState) in
+                NSNotificationCenter.defaultCenter().postNotification(progressionState.progressionNotification)
+            })
+            return self._reactiveHandlers!
+        }
+    }
+    
+}
---
+++ Shared/core/ProgressAndCompletionHandler.swift
@@ -0,0 +1,85 @@
+//
+//  ProgressAndCompletionHandler.swift
+//
+//  Created by Benoit Pereira da silva on 22/01/2016.
+//  Copyright © 2016 Benoit Pereira da silva. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: - TypeAliases
+
+//A composed Closure
+//with a progress and acompletion section
+public typealias ComposedProgressAndCompletionHandler = (progressionState:Progression?,completionState:Completion?)->()
+
+ //public typealias ComposedProgressAndCompletionHandler = (currentTaskIndex:Int,totalTaskCount:Int,currentTaskProgress:Double,message:String,data:NSData?,completed:Bool,success:Bool)->()
+
+//ProgressHandler
+public typealias ProgressHandler = (_: Progression) -> ()
+
+//CompletionHandler
+public typealias CompletionHandler = (_: Completion) -> ()
+
+
+// MARK: -
+
+//  Generally Used in XPC facades because we can pass only one handler per XPC call
+//  So we split the ComposedProgressAndCompletionHandler by calling ProgressAndCompletionHandler.handlersFrom(composed)
+@objc(ProgressAndCompletionHandler) public class ProgressAndCompletionHandler:NSObject{
+    
+    /// The progress handler
+    public var notify: ProgressHandler?
+    
+    public func addProgressHandler(progressHandler: ProgressHandler){
+        self.notify = progressHandler
+    }
+    /// The completion handler
+    public var on:(CompletionHandler)
+    
+    public required init(completionHandler: CompletionHandler){
+        self.on = completionHandler
+    }
+
+    /**
+     XPC adapter used to split closure in two parts
+     
+     - parameter composedHandler: the composed handler
+     - returns: an instance of ProgressAndCompletionHandler
+     */
+    public static func handlersFrom(composedHandler:ComposedProgressAndCompletionHandler)->ProgressAndCompletionHandler{
+
+        // Those handlers produce an adaptation
+        // From the unique handler form
+        // progress and completion handlers.
+        
+        let handlers=ProgressAndCompletionHandler {(onCompletion) -> () in
+            composedHandler(progressionState:nil,completionState:onCompletion)
+        }
+        
+        handlers.addProgressHandler { (onProgression) -> () in
+            composedHandler(progressionState:onProgression,completionState:nil)
+        }
+        return handlers
+    }
+    
+    // We need to provide a unique handler to be compatible with the XPC context
+    // So we use an handler adapter that relays to the progress and completion handlers
+    // to mask the constraint.
+    public func composedHandlers() -> ComposedProgressAndCompletionHandler {
+        let handler: ComposedProgressAndCompletionHandler = {(progressionState,completionState)-> Void in
+            if let progressionState=progressionState{
+                 self.notify?(progressionState)
+            }
+            if let completion=completionState{
+                 self.on(completion)
+            }
+        }
+        return handler
+    }
+}
+
+
+
+
