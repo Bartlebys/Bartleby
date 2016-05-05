00374d2b9085e5e8631e3fd54c29ba8f7d648537
Martin Delille <martin@phonations.com>
2016-05-05 16:59:00 +0200

    Script cleaning, building and testing all schemes

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Make the script to on error
+set -e
+
+xcodebuild -workspace Bartleby.xcworkspace -scheme "BsyncXPC" clean build
+xcodebuild -workspace Bartleby.xcworkspace -scheme "bsync" clean build test
+xcodebuild -workspace Bartleby.xcworkspace -scheme "Bartleby OSX" clean build test
+xcodebuild -workspace Bartleby.xcworkspace -scheme "Bartleby iOS" clean build test
+xcodebuild -workspace Bartleby.xcworkspace -scheme "bartleby" clean build
