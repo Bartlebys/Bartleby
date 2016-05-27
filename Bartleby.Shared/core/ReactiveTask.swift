39bc85549315e6c1a6c5a93cf3ba5eabf6deb6ef
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2016-05-27 11:59:25 +0200

    ReactiveTask / RelayingTask necessary re-introduction

--- Bartleby.Shared/core/ReactiveTask.swift
+++ Bartleby.Shared/core/ReactiveTask.swift
@@ -8,12 +8,7 @@
 
 import Foundation
 
-// @bpds not sure it is relevant it can introduce miss understanding.
-// completion and progression is group oriented.
-// Implementation is commented before deletion
-
-/*
-
+// MARK: ReactiveTask
 
 // A Reactive Task that allows to append handlers.
 @objc(ReactiveTask) public class  ReactiveTask: Task, Reactive {
@@ -28,6 +23,40 @@ import Foundation
 
 }
 
+// MARK: RelayingTask
+
+// A Reactive Task that relays it completion an progression.
+@objc(RelayingTask) public class  RelayingTask: Task, Reactive {
+
+    // Universal type support
+    override public class func typeName() -> String {
+        return "TaskWithNotifications"
+    }
+
+    // MARK: Reactive Handlers
+
+    private var _reactiveHandlers: Handlers?
+
+    // The reactive Handlers
+    public var reactiveHandlers: Handlers {
+        get {
+            if let _ = _reactiveHandlers {
+                return self._reactiveHandlers!
+            }
+            let onCompletion: CompletionHandler = { (completionState) in
+                self.complete(completionState)
+            }
+            self._reactiveHandlers=Handlers(completionHandler: onCompletion)
+            self._reactiveHandlers!.appendProgressHandler({ (progressionState) in
+                self.forward(progressionState)
+            })
+            return self._reactiveHandlers!
+        }
+    }
+}
+
+
+// MARK: TaskWithNotifications
 
 // Reactive Task that relay the completion and progression by NSNotifications
 @objc(TaskWithNotifications) public class  TaskWithNotifications: Task, Reactive {
@@ -74,5 +103,4 @@ import Foundation
         })
     }
 
-
-}*/
+}
