980857b1980326a6c2ee91f9a9aa24ec37d32e40
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2016-05-27 11:03:30 +0200

    ConcreteTasks guideline and samples enforcements.

--- Bartleby.Shared/core/protocols/ConcreteTask.swift
+++ Bartleby.Shared/core/protocols/ConcreteTask.swift
@@ -8,46 +8,63 @@
 
 import Foundation
 
-public protocol Invocable: Collectible {
-
-    /**
-        Runs the invocation on the Group Dispatch Queue
-        All the logic is encapuslated.
-        You must call super.invoke() and forward() on completion
-     */
-    func invoke()
-
-
-    /**
-     (!) This method is implemented as final in Task Extension to guarantee the task scheduler consistency
-     You must call this method when the task is completed.
-
-     it will forward the state on the main queue.
-
-     - parameter completionState: the completion state
-     */
-    func forward<T: ForwardableState>(state: T)
-
-}
-
-
-public protocol ForwardableState {
-}
-
+// MARK: Concrete Task protocol
 
 /*
 
- To implement a concreteTask :
+ # A concrete task can be executed in a TasksGroup by a TaskScheduler
 
- 1. Inheritate from Task or ReactiveTask.
- 2. You MUST define an associated type.
+ Task are implemented using GCD, and not NSOperationQueue.
+ They are not committed to light footprint or performance
+
+ Tasks are suitable for :
+
+ - concurent graph execution
+ - supervised monitoring
+ - interruptibility (serialization & deserialization of the process)
+ - distribution ( run on multiple nodes)
+ - ...
+
+
+ To obtain those goals you must conform to strict implementation rules.
+
+ # How to implement a concreteTask ?
+
+ 1. Your concrete task must be `Invocable`(Inheritate from Task or ReactiveTask).
+ 2. You MUST define the associated ArgumentType.
  3. The convenience initializer MUST Call self.configureWithArguments(arguments)
-    E.g:    convenience required public init(arguments: ArgumentType) {
-                self.init()
-                self.configureWithArguments(arguments)
+
+```
+convenience required public init(arguments: ArgumentType) {
+    self.init()
+    self.configureWithArguments(arguments)
+}
+```
+
+ 4. On invoke()
+ 4.1 call `super.invoke()`
+ 4.2 try to deserialize the arguments `let user: ArgumentType = try self.arguments() as ArgumentType`
+ 4.3 on any case call `self.complete(completionState)`
+
+```
+    override public func invoke()  {
+        super.invoke()
+        do{
+            if let user: ArgumentType = try self.arguments() as ArgumentType {
+                // do something
+                // On success call : `self.complete(Completion.successState())`
+                // On failure call : `self.complete(completionState)`
+                // You can  call : `forward(progressionState)` if the task is long
             }
 
+            }catch{
+                self.complete(Completion.failureState("Argument type missmatch", statusCode: CompletionStatus.Precondition_Failed))
+            }
+        }
+    }
+```
 
+ You can check implementations samples in the unit test or analyze `PushOperationTask` and `CommitAndPushPendingOperationsTask`
 
  */
 public protocol ConcreteTask: Invocable {
@@ -72,9 +89,47 @@ public protocol ConcreteTask: Invocable {
     init(arguments: ArgumentType)
 
 
-
     // (!) This method is implemented as final in a Task extension to force Type Matching Safety
     // it throws Task.ArgumentsTypeMisMatch
     func arguments<ExpectedType: Serializable>() throws -> ExpectedType
 
+}
+
+
+
+// MARK: Invocable protocol
+
+public protocol Invocable: Collectible {
+
+    /**
+        Runs the invocation on the Group Dispatch Queue
+        All the logic is encapuslated.
+     */
+    func invoke()
+
+
+    /**
+     Register the state and forwards the completion.
+     **Important!** During invocation you must in any cases call this method (on success or failure)
+
+     - parameter state: the completion state
+     */
+    func complete(state: Completion)
+
+    /**
+     (!) This method is implemented as final in Task Extension to guarantee the task scheduler consistency
+     This method is called by `complete()` when the task is completed.
+
+     it will forward the state on the main queue.
+
+     - parameter state: the Forwardable state
+     */
+    func forward<T: ForwardableState>(state: T)
+
+
+}
+
+// MARK: Invocable protocol
+
+public protocol ForwardableState {
 }
