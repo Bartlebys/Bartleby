18f5266112661b0ecbdba16b17e86efc2e582f33
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2016-08-09 19:35:41 +0200

    Progression UI skeleton

--- Bartleby.OSX-Apps/ProgressWindowController.swift
+++ Bartleby.OSX-Apps/ProgressWindowController.swift
@@ -2,30 +2,37 @@
 //  ProgressWindowController.swift
 //  BartlebyKit
 //
-//  Created by Benoit Pereira da silva on 03/08/2016.
+//  Created by Benoit Pereira da silva on 09/08/2016.
 //
 //
 
 import Cocoa
 
-class ProgressWindowController: NSWindowController,RegistryDependent {
+
+public class ProgressWindowController: NSWindowController,RegistryDependent {
 
 
-    override func windowDidLoad() {
+    //MARK : Window
+
+    override public func windowDidLoad() {
         super.windowDidLoad()
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
 
+    override public var windowNibName: String?{
+        return "ProgressWindowController"
+    }
+
+    @IBOutlet var arrayController: NSArrayController!
 
     dynamic var tasksGroups:[TasksGroup]?
 
-    internal var registryDelegate: RegistryDelegate?{
+    public var registryDelegate: RegistryDelegate?{
         didSet{
             if let registry=self.registryDelegate?.getRegistry(){
                 self.tasksGroups=registry.tasksGroups.items
             }
         }
     }
-
+    
     
 }
