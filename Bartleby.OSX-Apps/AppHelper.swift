2a29ca076b5cf6ba970175e85478d5daa0739d88
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2016-10-29 11:06:16 +0200

    Added an Helper to Present the last Operation metrics

--- Bartleby.OSX-Apps/AppHelper.swift
+++ Bartleby.OSX-Apps/AppHelper.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Cocoa
 
 
 public struct AppHelper {
@@ -21,6 +22,25 @@ public struct AppHelper {
         let metricsViewController=MetricsDetailsViewController(nibName: "MetricsDetailsViewController", bundle:  Bundle(for: MetricsDetailsViewController.self))!
         metricsViewController.metrics=metrics
         return metricsViewController
+    }
+
+
+    /// Presents a popover with the last Metrics
+    ///
+    /// - parameter presenter: the presenter
+    /// - parameter sender:    the sender for example a button
+    /// - parameter document:  the document reference.
+    public static func displayLastMetrics(presenter:NSViewController,sender:NSView,document:BartlebyDocument){
+        let metrics=document.metrics
+            if let lastMetrics=metrics.last{
+                let metricsViewController=AppHelper.getMetricsDetailsViewController(for: lastMetrics)
+                let frame = sender.frame
+                presenter.presentViewController( metricsViewController,
+                                            asPopoverRelativeTo: frame,
+                                            of: presenter.view,
+                                            preferredEdge:NSRectEdge(rawValue: 2)!,
+                                            behavior: NSPopoverBehavior.transient)
+            }
     }
 
 }
