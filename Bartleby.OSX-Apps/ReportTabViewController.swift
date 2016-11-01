3d4b34b84e35525843c2f1f21f5ee910cfa4f2ba
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2016-11-01 09:02:51 +0100

    Report supports Metadata details

---
+++ Bartleby.OSX-Apps/ReportTabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ReportTabViewController.swift
+//  BartlebyKit
+//
+//  Created by Benoit Pereira da silva on 01/11/2016.
+//
+//
+
+import Cocoa
+
+class ReportTabViewController: NSTabViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
