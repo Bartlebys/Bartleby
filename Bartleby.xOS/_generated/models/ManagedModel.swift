a77dd1dbf77ea8d6c97e41fbc4a3dd6dbcefe894
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2016-12-20 15:44:27 +0100

    Erasure cascade

--- Bartleby.xOS/_generated/models/ManagedModel.swift
+++ Bartleby.xOS/_generated/models/ManagedModel.swift
@@ -105,7 +105,6 @@ import Foundation
     // The Run time Type name (can be different to typeName)
     internal var _runTimeTypeName: String?
 
-
     /// Erases globally the instance and its dependent relations.
     /// Throws ErasingError and DocumentError
     /// You may Override this method to purge files (e.g: Node, Block, ...)
@@ -113,43 +112,63 @@ import Foundation
     /// - Parameter commit: set to true by default (we do not commit triggered Deletion)
     public func erase(commit:Bool=true)throws->(){
         if let document=self.referentDocument{
-            var toBeErased:[ManagedModel]=[self]
             var erasableUIDS:[String]=[self.UID]
-            func __append(_ UID:String)throws->(){
+
+            // Erase recursively
+            func __stageForErasure(_ UID:String)throws->(){
                 if !erasableUIDS.contains(UID){
-                    let target:ManagedModel = try Bartleby.registredObjectByUID(UID)
-                    toBeErased.append(target)
                     erasableUIDS.append(UID)
+                    let target:ManagedModel = try Bartleby.registredObjectByUID(UID)
+                    try target.erase(commit: commit)
                 }
             }
             for relation in self._relations{
-                if relation.relationship == Relation.Relationship.fusional
-                    || relation.relationship == Relation.Relationship.owns{
-                    try __append(relation.UID)
-                }
-                if relation.relationship == Relation.Relationship.coOwns{
+                switch relation.relationship {
+                case Relation.Relationship.free:
+                    // That's FreeDom! There is nothing to do
+                    break
+                case Relation.Relationship.owns:
+                    try __stageForErasure(relation.UID)
+                    break
+                case Relation.Relationship.ownedBy:
+                    // Remove the homologous relation
+                    if let object:ManagedModel = try? Bartleby.registredObjectByUID(relation.UID){
+                        object.removeRelation(Relation.Relationship.owns, to:self)
+                    }
+                    break
+                case Relation.Relationship.coOwns:
+                    // Count the owners to define if the object should be erased.
                     let m:[ManagedModel] = self.relations(Relation.Relationship.coOwns)
                     if m.count == 1{
-                        try __append(relation.UID)
+                        try __stageForErasure(relation.UID)
                     }
+                case Relation.Relationship.coOwnedBy:
+                    // Remove the homologous relation
+                    if let object:ManagedModel = try? Bartleby.registredObjectByUID(relation.UID){
+                        object.removeRelation(Relation.Relationship.coOwns, to:self)
+                    }
+                    break
+                case Relation.Relationship.fusional:
+                    // Prevent Circularity
+                    if let object:ManagedModel = try? Bartleby.registredObjectByUID(relation.UID){
+                        object.removeRelation(Relation.Relationship.fusional, to:self)
+                    }
+                    try __stageForErasure(relation.UID)
+                    break
                 }
-                // There is nothing to do for:
-                // - Relation.Relationship.free
-                // - Relation.Relationship.ownedBy
-                // - Relation.Relationship.coOwnedBy
             }
-            for erasable in toBeErased{
-                // Erase from the collection
-                if let collection=document.collectionByName(erasable.d_collectionName) as? CollectibleCollection {
-                    collection.removeObject(self, commit:commit)
-                    // Unregister the ManagedModel
-                    Bartleby.unRegister(erasable)
-                }
+            // Erase from managed collection
+            if let collection=document.collectionByName(self.d_collectionName) as? CollectibleCollection {
+                collection.removeObject(self, commit:commit)
+                // Unregister the ManagedModel
+                Bartleby.unRegister(self)
             }
+
         }else{
             throw ErasingError.referentDocumentUndefined
         }
     }
+
 
     // MARK: - Exposed (Bartleby's KVC like generative implementation)
 
