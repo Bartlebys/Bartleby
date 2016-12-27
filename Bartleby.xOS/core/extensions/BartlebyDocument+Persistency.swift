6fc48f1a13327b21a2638786799af25721f1dfd3
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2016-12-27 12:15:57 +0100

    Added Bookmark support to block to allow XPC services to repackage blocks within another Document

--- Bartleby.xOS/core/extensions/BartlebyDocument+Persistency.swift
+++ Bartleby.xOS/core/extensions/BartlebyDocument+Persistency.swift
@@ -47,16 +47,16 @@ extension BartlebyDocument{
 
     // To Read content
     open override func load(fromContents contents: Any, ofType typeName: String?) throws {
-        if let fileWrapper = contents as? FileWrapper{
-            try self._read(from:fileWrapper)
-        }else{
-            throw DocumentError.fileWrapperNotFound(message:"on load")
-        }
+    if let fileWrapper = contents as? FileWrapper{
+    try self._read(from:fileWrapper)
+    }else{
+    throw DocumentError.fileWrapperNotFound(message:"on load")
+    }
     }
 
     // To Write content
     override open func contents(forType typeName: String) throws -> Any {
-        return try self._updatedFileWrappers()
+    return try self._updatedFileWrappers()
     }
 
     #endif
@@ -151,7 +151,7 @@ extension BartlebyDocument{
                     proxy.propagate()
                 }
             }
-            
+
             Async.main{
                 self.documentDidLoad()
             }
@@ -163,95 +163,95 @@ extension BartlebyDocument{
 
     private func _updatedFileWrappers()throws ->FileWrapper{
         self.documentWillSave()
-            if var fileWrappers=self.documentFileWrapper.fileWrappers {
+        if var fileWrappers=self.documentFileWrapper.fileWrappers {
 
-                // ##############
-                // # Metadata
-                // ##############
+            // ##############
+            // # Metadata
+            // ##############
 
-                #if os(OSX)
-                    // Try to store a preferred filename
-                    self.metadata.preferredFileName=self.fileURL?.lastPathComponent
-                #else
-                    // Try to store a preferred filename
-                    self.metadata.preferredFileName=self.fileURL.lastPathComponent
-                #endif
+            #if os(OSX)
+                // Try to store a preferred filename
+                self.metadata.preferredFileName=self.fileURL?.lastPathComponent
+            #else
+                // Try to store a preferred filename
+                self.metadata.preferredFileName=self.fileURL.lastPathComponent
+            #endif
 
-                var metadataData=self.metadata.serialize()
-                metadataData = try Bartleby.cryptoDelegate.encryptData(metadataData)
+            var metadataData=self.metadata.serialize()
+            metadataData = try Bartleby.cryptoDelegate.encryptData(metadataData)
 
-                // Remove the previous metadata
-                if let wrapper=fileWrappers[self._metadataFileName] {
-                    self.documentFileWrapper.removeFileWrapper(wrapper)
-                }
-                let metadataFileWrapper=FileWrapper(regularFileWithContents: metadataData)
-                metadataFileWrapper.preferredFilename=self._metadataFileName
-                self.documentFileWrapper.addFileWrapper(metadataFileWrapper)
+            // Remove the previous metadata
+            if let wrapper=fileWrappers[self._metadataFileName] {
+                self.documentFileWrapper.removeFileWrapper(wrapper)
+            }
+            let metadataFileWrapper=FileWrapper(regularFileWithContents: metadataData)
+            metadataFileWrapper.preferredFilename=self._metadataFileName
+            self.documentFileWrapper.addFileWrapper(metadataFileWrapper)
 
-                // ##############
-                // # BSFS DATA
-                // ##############
+            // ##############
+            // # BSFS DATA
+            // ##############
 
-                if let wrapper=fileWrappers[self._bsfsDataFileName]{
-                    self.documentFileWrapper.removeFileWrapper(wrapper)
-                }
+            if let wrapper=fileWrappers[self._bsfsDataFileName]{
+                self.documentFileWrapper.removeFileWrapper(wrapper)
+            }
 
-                let data = try Bartleby.cryptoDelegate.encryptData(self.bsfs.saveState())
-                let bsfsFileWrapper=FileWrapper(regularFileWithContents:data)
-                bsfsFileWrapper.preferredFilename=self._bsfsDataFileName
-                self.documentFileWrapper.addFileWrapper(bsfsFileWrapper)
+            let data = try Bartleby.cryptoDelegate.encryptData(self.bsfs.saveState())
+            let bsfsFileWrapper=FileWrapper(regularFileWithContents:data)
+            bsfsFileWrapper.preferredFilename=self._bsfsDataFileName
+            self.documentFileWrapper.addFileWrapper(bsfsFileWrapper)
 
 
-                // ##############
-                // # Collections
-                // ##############
+            // ##############
+            // # Collections
+            // ##############
 
-                for metadatum: CollectionMetadatum in self.metadata.collectionsMetadata {
+            for metadatum: CollectionMetadatum in self.metadata.collectionsMetadata {
 
-                    if !metadatum.inMemory {
-                        let collectionfileName=self._collectionFileNames(metadatum).crypted
-                        // MONOLITHIC STORAGE
-                        if metadatum.storage == CollectionMetadatum.Storage.monolithicFileStorage {
+                if !metadatum.inMemory {
+                    let collectionfileName=self._collectionFileNames(metadatum).crypted
+                    // MONOLITHIC STORAGE
+                    if metadatum.storage == CollectionMetadatum.Storage.monolithicFileStorage {
 
-                            if var collection = self.collectionByName(metadatum.collectionName) as? CollectibleCollection {
+                        if var collection = self.collectionByName(metadatum.collectionName) as? CollectibleCollection {
 
-                                if collection.shouldBeSaved{
+                            if collection.shouldBeSaved{
 
-                                    // We use multiple files
-                                    // The resulting data is not a valid String check CryptoDelegate for details.
-                                    let collectionString = collection.serializeToUFf8String()
-                                    let collectionData = try Bartleby.cryptoDelegate.encryptStringToData(collectionString)
+                                // We use multiple files
+                                // The resulting data is not a valid String check CryptoDelegate for details.
+                                let collectionString = collection.serializeToUFf8String()
+                                let collectionData = try Bartleby.cryptoDelegate.encryptStringToData(collectionString)
 
-                                    // Remove the previous data
-                                    if let wrapper=fileWrappers[collectionfileName] {
-                                        self.documentFileWrapper.removeFileWrapper(wrapper)
-                                    }
-
-                                    let collectionFileWrapper=FileWrapper(regularFileWithContents: collectionData)
-                                    collectionFileWrapper.preferredFilename=collectionfileName
-                                    self.documentFileWrapper.addFileWrapper(collectionFileWrapper)
-
-                                    // Reinitialize the flag
-                                    collection.shouldBeSaved=false
+                                // Remove the previous data
+                                if let wrapper=fileWrappers[collectionfileName] {
+                                    self.documentFileWrapper.removeFileWrapper(wrapper)
                                 }
 
-                            } else {
-                                // NO COLLECTION
-                            }
-                        } else {
-                            // INCREMENTAL STORAGE CURRENTLY NOT SUPPORTED
-                        }
-                    }
-                }
+                                let collectionFileWrapper=FileWrapper(regularFileWithContents: collectionData)
+                                collectionFileWrapper.preferredFilename=collectionfileName
+                                self.documentFileWrapper.addFileWrapper(collectionFileWrapper)
 
-                // Bsfs blocks
-                if  fileWrappers[self.blocksDirectoryWrapperName] == nil{
-                    let blocksFileWrapper=FileWrapper(directoryWithFileWrappers: [:])
-                    blocksFileWrapper.preferredFilename=self.blocksDirectoryWrapperName
-                    self.documentFileWrapper.addFileWrapper(blocksFileWrapper)
+                                // Reinitialize the flag
+                                collection.shouldBeSaved=false
+                            }
+
+                        } else {
+                            // NO COLLECTION
+                        }
+                    } else {
+                        // INCREMENTAL STORAGE CURRENTLY NOT SUPPORTED
+                    }
                 }
             }
 
+            // Bsfs blocks
+            if  fileWrappers[self.blocksDirectoryWrapperName] == nil{
+                let blocksFileWrapper=FileWrapper(directoryWithFileWrappers: [:])
+                blocksFileWrapper.preferredFilename=self.blocksDirectoryWrapperName
+                self.documentFileWrapper.addFileWrapper(blocksFileWrapper)
+            }
+        }
+        
         return self.documentFileWrapper
     }
     
