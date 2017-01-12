51dca16cd8df1b2137354e3002cd9092a2015aae
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2017-01-12 18:53:22 +0100

    Serialization of sugar

--- Bartleby.xOS/_generated/models/DocumentMetadata.swift
+++ Bartleby.xOS/_generated/models/DocumentMetadata.swift
@@ -129,6 +129,7 @@ import Foundation
 			self.spaceUID <- ( map["spaceUID"] )
 			self.persistentUID <- ( map["persistentUID"] )
 			self.currentUserUID <- ( map["currentUserUID"] )
+			self.sugar <- ( map["sugar"] )
 			self.identificationMethod <- ( map["identificationMethod"] )
 			self.appGroup <- ( map["appGroup"] )
 			self.identificationValue <- ( map["identificationValue"] )
@@ -162,6 +163,7 @@ import Foundation
 			self.spaceUID=String(describing: decoder.decodeObject(of: NSString.self, forKey: "spaceUID")! as NSString)
 			self.persistentUID=String(describing: decoder.decodeObject(of: NSString.self, forKey: "persistentUID")! as NSString)
 			self.currentUserUID=String(describing: decoder.decodeObject(of: NSString.self, forKey: "currentUserUID")! as NSString)
+			self.sugar=String(describing: decoder.decodeObject(of: NSString.self, forKey: "sugar")! as NSString)
 			self.identificationMethod=DocumentMetadata.IdentificationMethod(rawValue:String(describing: decoder.decodeObject(of: NSString.self, forKey: "identificationMethod")! as NSString))! 
 			self.appGroup=String(describing: decoder.decodeObject(of: NSString.self, forKey:"appGroup") as NSString?)
 			self.identificationValue=String(describing: decoder.decodeObject(of: NSString.self, forKey:"identificationValue") as NSString?)
@@ -191,6 +193,7 @@ import Foundation
 		coder.encode(self.spaceUID,forKey:"spaceUID")
 		coder.encode(self.persistentUID,forKey:"persistentUID")
 		coder.encode(self.currentUserUID,forKey:"currentUserUID")
+		coder.encode(self.sugar,forKey:"sugar")
 		coder.encode(self.identificationMethod.rawValue ,forKey:"identificationMethod")
 		if let appGroup = self.appGroup {
 			coder.encode(appGroup,forKey:"appGroup")
