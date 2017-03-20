eb92c6935333601d01f74bfc24cb14b573c1c1b9
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2017-03-20 17:44:41 +0100

    View Recycler reset

--- BartlebysUI/BartlebysUI.xOS/ViewsRecycler.swift
+++ BartlebysUI/BartlebysUI.xOS/ViewsRecycler.swift
@@ -8,47 +8,86 @@
 
 import Foundation
 
+
 open class ViewsRecycler {
 
-    struct ViewStatus {
+    class ViewReferer {
+
         var view:BXView
-        var classifier:String
+        var groupName:String
         var available:Bool
+
+        public init(view: BXView, groupName:String, available:Bool = false){
+            self.view = view
+            self.groupName = groupName
+            self.available = available
+        }
     }
-    // Classifyed recyclable views
-    fileprivate var _recyclableViews=[String:[BXView]]()
+
+
 
     // We keep a reference on all views.
-    fileprivate var _viewsStatus=[ViewStatus]()
+    var _viewsReferers=[ViewReferer]()
 
     // You can for performance tuning determine a min distance to determine what should be considerated as recyclable
     var minOffScreenDistance:CGFloat = 100
+
 
     public init() {}
 
     /// Purges all the views (e.g on view controller deinit)
     open func purgeViews(){
-        while let vs = self._viewsStatus.popLast(){
+        while let vs = self._viewsReferers.popLast(){
             vs.view.removeFromSuperview()
         }
-        self._recyclableViews.removeAll()
+    }
+
+    open func stats()->(total:Int,used:Int, available:Int){
+        let total = self._viewsReferers.count
+        var used = 0
+        var available = 0
+        for vs in self._viewsReferers{
+            if vs.available {
+                available += 1
+            }else{
+                used += 1
+            }
+        }
+        return (total,used,available)
     }
 
     /// You must call this method regularly to recycle off screen views.
     open func recycleRecyclableViews(){
-        for vs in self._viewsStatus{
+        for vs in self._viewsReferers{
             if let superview = vs.view.superview{
                 if  vs.view.frame.origin.x + vs.view.frame.width + self.minOffScreenDistance < superview.frame.origin.x ||
                     vs.view.frame.origin.x > superview.frame.origin.x + superview.frame.width + self.minOffScreenDistance ||
                     vs.view.frame.origin.y + vs.view.frame.height + self.minOffScreenDistance < superview.frame.origin.y ||
                     vs.view.frame.origin.y > superview.frame.origin.y + superview.frame.height + self.minOffScreenDistance {
-                    self._recycleView(view: vs.view, classifiedBy:vs.classifier)
+                    let _ = self.recycleView(view: vs.view)
                 }
             }else if vs.available == false{
                 // The clients may remove the view from the superview to force the recycling
-                self._recycleView(view: vs.view, classifiedBy:vs.classifier)
+                self.recycleView(view: vs.view)
             }
         }
+    }
+
+
+    /// Recycles explicitely a view
+    ///
+    /// - Parameters:
+    ///   - view: the view to recycle
+    open func recycleView(view:BXView)->Bool{
+        // Update the view status
+        if let vs = self._viewsReferers.first(where: { (viewStatus) -> Bool in
+            return viewStatus.view == view
+        }){
+            view.removeFromSuperview()
+            vs.available = true
+            return true
+        }
+        return false
     }
 
 
@@ -58,39 +97,34 @@ open class ViewsRecycler {
     ///   - identifiedBy: and identifier shared between a group of views
     ///   - viewFactory:  the factory method to create a new view
     /// - Returns: a recyclable view
-    open func getARecyclableView(identifiedBy:String,viewFactory:()->(BXView))->BXView{
-        if let view = self._recyclableViews[identifiedBy]?.popLast(){
-            return view
+    open func getARecyclableView(groupName:String,viewFactory:()->(BXView))->BXView{
+        if let referer = self._viewsReferers.first(where:{ (referer) -> Bool in
+            return (referer.available && referer.groupName==groupName)
+        }){
+            referer.available = false
+            return referer.view
         }
         let view = viewFactory()
-        let vs = ViewStatus(view: view, classifier:identifiedBy, available: false)
-        self._viewsStatus.append(vs)
+        let vs = ViewReferer(view: view, groupName:groupName, available: false)
+        self._viewsReferers.append(vs)
         return view
     }
 
-    // MARK: - Private implementation
 
-    /// Recycles a view
+    /// Return all the actives view grouped by the groupname
     ///
-    /// - Parameters:
-    ///   - view: the view to recycle
-    ///   - groupedBy: its group
-    fileprivate func _recycleView(view:BXView,classifiedBy:String){
-        var group:[BXView]!
-        if let existingGroup=_recyclableViews[classifiedBy] {
-            group = existingGroup
-        }else{
-            group = [BXView]()
-        }
-        group.append(view)
-        view.removeFromSuperview()
-        self._recyclableViews[classifiedBy]=group
-        // Update the view status
-        if var vs = self._viewsStatus.first(where: { (viewStatus) -> Bool in
-            return viewStatus.view == view
-        }){
-            vs.available = true
-        }
+    /// - Parameter groupName: the classifier
+    /// - Returns: the collection of actives views
+    open func getAllActiveViews(groupedBy groupName:String)->[BXView]{
+        return self._viewsReferers.flatMap({ (referer) -> BXView? in
+            if  (referer.groupName == groupName && !referer.available){
+                return referer.view
+            }else{
+                return nil
+            }
+        })
     }
+
+
 
 }
