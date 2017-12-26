d74747592e548414940af0f5de93d360048225c0
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2017-12-26 16:15:41 +0100

    Added global func equalityOf(l,r) to fix some Equatable issues

--- Bartleby.xOS/core/extensions/ManagedModel+Equatable.swift
+++ Bartleby.xOS/core/extensions/ManagedModel+Equatable.swift
@@ -10,8 +10,21 @@ import Foundation
 
 // MARK: - Equatable
 
+// We have encountered serious QUIRKS With `Equatable` + OBJC runtime (with swift 4)
+// Such inconsistencies or bugs where very difficult to debug o
+// So we have decided to create global functions equalityOf(...)
+// The Equatable implementation relies on this function
+
+
 extension ManagedModel{
     public static func ==(lhs: ManagedModel, rhs: ManagedModel) -> Bool {
-        return lhs.UID==rhs.UID
+        return equalityOf(lhs, rhs)
     }
 }
+
+public func equalityOf(_ lhs: ManagedModel?, _ rhs: ManagedModel?)-> Bool {
+    let equality = lhs?.UID == rhs?.UID
+    return equality
+}
+
+
