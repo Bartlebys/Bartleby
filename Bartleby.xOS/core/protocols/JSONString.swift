9f3d933033c0efe11c2310f84c2f293173c52c40
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-06-05 17:01:13 +0200

    Fixes Bartleby CLI embedded modules compilation issues

--- Bartleby.xOS/core/protocols/JSONString.swift
+++ Bartleby.xOS/core/protocols/JSONString.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 
-public protocol JSONString:CustomStringConvertible{
+public protocol JSONString: CustomStringConvertible{
     func toJSONString(_ prettyPrint:Bool)->String
 }
