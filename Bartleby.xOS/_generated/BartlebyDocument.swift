90ef1fc3582d6a4eb87c17ffec57d8ac76ca84d0
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-06-15 16:19:12 +0200

    Regeneration

--- Bartleby.xOS/_generated/BartlebyDocument.swift
+++ Bartleby.xOS/_generated/BartlebyDocument.swift
@@ -98,22 +98,22 @@ import Foundation
         return self._bsfs!
     }
 
-    internal var _bsfs:BSFS?
+    internal var _bsfs: BSFS?
 
     // Hook the triggers
-    public var triggerHooks=[TriggerHook]()
+    public var triggerHooks = [TriggerHook]()
 
     // Triggered Data is used to store data before data integration
-    internal var _triggeredDataBuffer:[Trigger]=[Trigger]()
+    internal var _triggeredDataBuffer:[Trigger] = [Trigger]()
 
     // An in memory flag to distinguish dotBart import case
-    open var dotBart=false
+    open var dotBart = false
 
     /// The underlining storage hashed by collection name
-    internal var _collections=[String:BartlebyCollection]()
+    internal var _collections = [String:BartlebyCollection]()
 
     /// We store the URL of the active security bookmarks
-    internal var _activeSecurityBookmarks=[String:URL]()
+    internal var _activeSecurityBookmarks = [String:URL]()
 
     // Reachability Manager
     internal var _reachabilityManager:NetworkReachabilityManager?
@@ -122,23 +122,23 @@ import Foundation
 
     // The online flag is driving the "connection" process
     // It connects to the SSE and starts the pushLoop
-    open var online:Bool=false{
+    open var online:Bool = false{
         willSet{
             // Transition on line
-            if newValue==true && online==false{
+            if newValue == true && online == false{
                 self._connectToSSE()
             }
             // Transition off line
-            if newValue==false && online==true{
+            if newValue == false && online == true{
                 self.log("SSE is transitioning offline",file:#file,function:#function,line:#line,category: "SSE")
                 self._closeSSE()
             }
-            if newValue==online{
+            if newValue == online{
                 self.log("Neutral online var setting",file:#file,function:#function,line:#line,category: "SSE")
             }
         }
         didSet{
-            self.metadata.online=online
+            self.metadata.online = online
             self.startPushLoopIfNecessary()
         }
     }
@@ -149,21 +149,21 @@ import Foundation
     internal var _sse:EventSource?
 
     // The EventSource URL for Server Sent Events
-    @objc open dynamic lazy var sseURL:URL=URL(string: self.baseURL.absoluteString+"/SSETriggers?spaceUID=\(self.spaceUID)&observationUID=\(self.UID)&lastIndex=\(self.metadata.lastIntegratedTriggerIndex)&runUID=\(Bartleby.runUID)&showDetails=false")!
+    @objc open dynamic lazy var sseURL:URL = URL(string: self.baseURL.absoluteString+"/SSETriggers?spaceUID=\(self.spaceUID)&observationUID=\(self.UID)&lastIndex=\(self.metadata.lastIntegratedTriggerIndex)&runUID=\(Bartleby.runUID)&showDetails=false")!
 
-    open var synchronizationHandlers:Handlers=Handlers.withoutCompletion()
+    open var synchronizationHandlers:Handlers = Handlers.withoutCompletion()
 
-    internal var _timer:Timer?
+    internal var _timer: Timer?
 
     // MARK: - Metrics
 
-    @objc open dynamic var metrics=[Metrics]()
+    @objc open dynamic var metrics = [Metrics]()
 
     // MARK: - Logs
 
-    open var enableLog: Bool=true
+    open var enableLog: Bool = true
 
-    open var printLogsToTheConsole: Bool=false
+    open var printLogsToTheConsole: Bool = true
 
     open var logs=[LogEntry]()
 
@@ -172,15 +172,15 @@ import Foundation
     // MARK: - Consignation
 
     /// The display duration of volatile messages
-    public static let VOLATILE_DISPLAY_DURATION: Double=3
+    public static let VOLATILE_DISPLAY_DURATION: Double = 3
 
     // MARK:  Simple stack management
 
-    open var trackingIsEnabled: Bool=false
+    open var trackingIsEnabled: Bool = false
 
-    open var glogTrackedEntries: Bool=false
+    open var glogTrackedEntries: Bool = false
 
-    open var trackingStack=[(result:Any?, context:Consignable)]()
+    open var trackingStack = [(result:Any?, context:Consignable)]()
 
     // MARK: - BSFS: BoxDelegate
 
